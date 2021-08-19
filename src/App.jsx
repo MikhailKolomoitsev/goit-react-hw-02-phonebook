d5ce707d9be91d913ffaf85d9eb8c4fd2353c1e0
mihon.testosterone <mihon.testosterone@gmail.com>
2021-08-19 11:41:40 +0300

    something added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
-import React, { Component } from 'react';
-import { v4} from 'uuid';
+import "./App.css";
+import React, { Component } from "react";
+import { v4 } from "uuid";
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    name: "",
+  };
+
+  addContact() {
+    this.setState((prevState) => ({
+      contacts: this.state.contats,
+    }));
   }
 
   render() {
@@ -13,16 +24,16 @@ class App extends Component {
       <div className="App">
         <h2>Name</h2>
         <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-  required
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
         />
-        <button>Add contact</button>
-      </div>)
-    }
-  
+        <button type="button">Add contact</button>
+      </div>
+    );
+  }
 }
 
 export default App;
